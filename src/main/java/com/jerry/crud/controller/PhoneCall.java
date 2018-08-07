ed9015bc0b0a739ff77ba22e57a8072b9ab1120f
jerry <jerrybw@163.com>
2018-08-07 11:23:53 +0800

    加上工号限制

--- src/main/java/com/jerry/crud/controller/PhoneCall.java
+++ src/main/java/com/jerry/crud/controller/PhoneCall.java
@@ -1,7 +1,5 @@
 package com.jerry.crud.controller;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.log;
-
 import java.util.HashMap;
 import java.util.Map;
 
@@ -24,17 +22,19 @@ public class PhoneCall {
 	@ResponseBody
 	@RequestMapping("/call")
 	public Object call(String cno,String tel) {
-		String url = "https://api-test-2.cticloud.cn/interface/v10/previewOutcall";
 		Map<String, String> res = new HashMap<>();
-		Map<String, String> param = new HashMap<>();
-		param.put("validateType", "2");
-		param.put("enterpriseId", "7000002");
-		long timestamp = System.currentTimeMillis()/1000;
-		param.put("timestamp",timestamp + "");
-		param.put("sign", MD5Util.md5Password("7000002"+timestamp+"2ff82f103ba7aac0055da123d56168c6"));
-		param.put("cno", cno);
-		param.put("tel", tel);
-		HttpRequestUtil.sendPost(url, param);
+		if("4321".equals(cno) || "6008".equals(cno) || "4210".equals(cno)) {
+			String url = "https://api-test-2.cticloud.cn/interface/v10/previewOutcall";
+			Map<String, String> param = new HashMap<>();
+			param.put("validateType", "2");
+			param.put("enterpriseId", "7000002");
+			long timestamp = System.currentTimeMillis()/1000;
+			param.put("timestamp",timestamp + "");
+			param.put("sign", MD5Util.md5Password("7000002"+timestamp+"2ff82f103ba7aac0055da123d56168c6"));
+			param.put("cno", cno);
+			param.put("tel", tel);
+			HttpRequestUtil.sendPost(url, param);
+		}
 		return res;
 	}
 
