9f78de2d54eb8cf754cbf49c30a45716c11c7ee2
jerry <jerrybw@163.com>
2018-08-10 12:03:27 +0800

    修改逻辑

--- src/main/java/com/jerry/crud/controller/PhoneCall.java
+++ src/main/java/com/jerry/crud/controller/PhoneCall.java
@@ -28,11 +28,11 @@ public class PhoneCall {
 		if("4321".equals(cno) || "6008".equals(cno) || "4210".equals(cno)) {
 			Timer timer = new Timer();
 	        //延迟1000ms执行程序
-			System.out.println("休眠2s调用外呼接口");
+			System.out.println("启用新线程，2s后调用外呼接口。-------cno："+cno);
 	        timer.schedule(new TimerTask() {
 	        	 @Override
 		            public void run() {
-	        		 String url = "https://api-test-2.cticloud.cn/interface/v10/previewOutcall";
+	        		String url = "https://api-test-2.cticloud.cn/interface/v10/previewOutcall";
 	     			Map<String, String> param = new HashMap<>();
 	     			param.put("validateType", "2");
 	     			param.put("enterpriseId", "7000002");
@@ -42,11 +42,12 @@ public class PhoneCall {
 	     			param.put("cno", cno);
 	     			param.put("tel", tel);
 	     			String result = HttpRequestUtil.sendPost(url, param);
-	     			System.out.println(result);
+	     			System.out.println("调用外呼接口。----cno:"+cno+"-------结果："+result);
 	     			
 	     			Timer timer = new Timer();
 
-	     	        //延迟1000ms执行程序
+	     	        //启用新线程 63s后调用挂机接口
+	     			System.out.println("启用新线程，63s后调用挂机接口。-------cno："+cno);
 	     	        timer.schedule(new TimerTask() {
 	     	            @Override
 	     	            public void run() {
@@ -59,9 +60,9 @@ public class PhoneCall {
 	     	    			param.put("sign", MD5Util.md5Password("7000002"+timestamp+"2ff82f103ba7aac0055da123d56168c6"));
 	     	    			param.put("cno", cno);
 	     	    			String result = HttpRequestUtil.sendPost(url, param);
-	     	    			System.out.println(result);
+	     	    			System.out.println("调用挂机口。----cno:"+cno+"-------结果："+result);
 	     	            }
-	     	        }, 63000);
+	     	        }, 23000);
 	        	 }
 	        },2000);
 		}
