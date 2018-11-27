7c582c55a0099adf31430fc476e23642ae02cba2
jerry <jerrybw@163.com>
2018-11-27 17:35:03 +0800

    修改

--- src/main/java/com/jerry/crud/controller/TestController.java
+++ src/main/java/com/jerry/crud/controller/TestController.java
@@ -26,17 +26,19 @@ public class TestController {
 	@RequestMapping("/json")
 	public Object testJson(@RequestBody Map<String, Object> map,HttpServletRequest request) {
 		System.out.println("收到请求-------");
-		String[] strings = {"床前明月光","燕草如碧丝","疑是地上霜","秦桑低绿枝","举头望明月","当君怀归日","低头思故乡","是妾断肠时","死别已吞声","春风不相识","生别常恻恻","何事入罗帏"};
-//		Map<String, Object> parameterMap = request.getParameterMap();
+		String[] strings = {"床前明月光，疑是地上霜。举头望明月，低头思故乡。床前明月光，疑是地上霜。举头望明月，低头思故乡。床前明月光，疑是地上霜。举头望明月，低头思故乡。",
+				"燕草如碧丝，秦桑低绿枝。当君怀归日，是妾断肠时。春风不相识，何事入罗帏。","疑是地上霜","秦桑低绿枝","举头望明月","当君怀归日","低头思故乡","是妾断肠时","死别已吞声","春风不相识","生别常恻恻","何事入罗帏"};
+		Map<String, String> parameterMap = new HashMap<>();
 		Map<String, Object> res = new HashMap<>();
 		Set<Entry<String, Object>> entrySet = map.entrySet();
 		for (Entry<String, Object> entry : entrySet) {
 			System.out.println("key "+entry.getKey());
 			System.out.println("value "+entry.getValue());
 		}
-		res.put("type", "play");
+		res.put("type", "playWait");
 		Random random = new Random();
-		res.put("playObject",strings[random.nextInt(strings.length)]);
+		res.put("playObject",strings[random.nextInt(2)]);
+		res.put("variables",parameterMap);
 		res.put("playType", "4");
 		return res;
 	}
